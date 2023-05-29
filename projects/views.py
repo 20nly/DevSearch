47f99074367189dbbf86d7e26c564b25c839ffad
Grand <fgt0191@gmail.com>
2023-05-29 21:26:14 +0500

    2 Explaining how to work Jinja in Django

--- projects/views.py
+++ projects/views.py
@@ -5,10 +5,31 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Eccomerce Website',
+        'description': 'Fully functional ecommerce website',
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my portfolio',
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working '
+    }
+]
+
 
 def projects(request):
-    return render(request, 'projects.html')
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projectsList': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
