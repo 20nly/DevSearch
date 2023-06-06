036ccab35c1eabe567cd776a2f32dfe057f5be71
Grand <fgt0191@gmail.com>
2023-06-06 22:32:34 +0500

    3.3 added first variant to tags (incorrect), just for introduction

--- projects/views.py
+++ projects/views.py
@@ -4,37 +4,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.shortcuts import render
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Eccomerce Website',
-        'description': 'Fully functional ecommerce website',
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'This was a project where I built out my portfolio',
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'Awesome open source project I am still working '
-    }
-]
+from .models import Project
+# projectsList = [
+#     {
+#         'id': '1',
+#         'title': 'Eccomerce Website',
+#         'description': 'Fully functional ecommerce website',
+#     },
+#     {
+#         'id': '2',
+#         'title': 'Portfolio Website',
+#         'description': 'This was a project where I built out my portfolio',
+#     },
+#     {
+#         'id': '3',
+#         'title': 'Social Network',
+#         'description': 'Awesome open source project I am still working '
+#     }
+# ]
 
 
 def projects(request):
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number, 'projectsList': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
 
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    return render(request, 'projects/single-project.html',
+                  {'projectObj': projectObj, 'tags': tags})
