0deda88c561d1646bb00c42f397892220722681e
work-pc <fgt0191@gmail.com>
2023-06-15 17:16:37 +0500

    6 Added users app, settings.py, url, views.py, was updated links

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -23,7 +23,8 @@ from projects.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
+    path('', include('projects.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
