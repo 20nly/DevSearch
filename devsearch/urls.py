88cd266c2d8ee6c39cd8f10d16c2038683716d9f
work-pc <fgt0191@gmail.com>
2023-06-16 17:07:21 +0500

    6.1 Added users app, migrated, connected to admin settings.py models users

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -23,8 +23,8 @@ from projects.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
-    path('users/', include('users.urls'))
+    path('projects/', include('projects.urls')),
+    path('', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from users.models import Profile
 
 """
 Если поле имеет blank=True, проверка формы позволит ввести пустое значение.
@@ -8,6 +9,7 @@ from django.db import models
 
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
@@ -19,6 +21,7 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
 
     def __str__(self):
         return self.title
